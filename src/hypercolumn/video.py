2de402927ea825100e09a8352aef89c7c75ca485
Marc SANSELME <marc.sanselme@gmail.com>
2016-12-19 13:48:06 +0100

    video finalization

--- src/hypercolumn/video.py
+++ src/hypercolumn/video.py
@@ -1,5 +1,6 @@
 # coding: utf8
 from matplotlib import pyplot as plt
+from matplotlib import animation
 
 import cv2
 import numpy as np
@@ -15,9 +16,12 @@ model = VGG_16('vgg16_weights.h5')
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy')
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-out = cv2.VideoWriter('../../thresholded.avi',fourcc, 20.0, (224,224), 0)
+# Set up formatting for the movie files
+FFMpegWriter = animation.writers['ffmpeg']
+metadata = dict(title='Threshold test', artist='Steven Spielberg', comment='Rated 5 stars!')
+writer = FFMpegWriter(fps=15, metadata=metadata)
+fig = plt.figure()
+ims=[]
 
 # Capture the video
 cap = cv2.VideoCapture('../../pendule.mp4')
@@ -37,15 +41,18 @@ while (cap.isOpened()):
     # get features from the third layer
     feat = get_activations(model, 15, im)
     m = np.matrix((feat[0][0][7]>1000)*255.0, dtype=float)
-    out.write(np.uint8(m))
+    ims.append(m)
     f+=1
     print 'frame '+str(f)
-    if f>20:
-        break
+    # if f>20:
+    #     break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
-out.release()
+with writer.saving(fig, "../../threshold.mp4", 100):
+    for im in ims:
+        plt.imshow(im)
+        writer.grab_frame()
 cv2.destroyAllWindows()
 
