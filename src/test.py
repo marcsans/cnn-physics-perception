5d2e56cf1af487dffac5e8f7253758fe1635337c
Louis THIRY <louis.thiry@outlook.fr>
2017-01-07 11:14:22 +0100

    add test file

---
+++ src/test.py
@@ -0,0 +1,9 @@
+import pendulum
+
+print 'test simple pendulum'
+pendulum.run_simple_pendulum_example()
+
+print 'test double pendulum animation'
+pendulum.run_double_pendulum_example_with_animation()
+
+
