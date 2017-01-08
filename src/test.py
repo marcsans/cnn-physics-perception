89509cd0665446fa703da0d5e97af1903bf6a86b
Marc SANSELME <marc.sanselme@gmail.com>
2017-01-08 16:39:05 +0100

    angles

--- src/test.py
+++ src/test.py
@@ -3,11 +3,11 @@ import numpy as np
 import learn_pendulum_parameter
 
 print 'test simple pendulum'
-pendulum.run_simple_pendulum_example(l=1.0)
-pendulum.run_simple_pendulum_example(l=2.0)
+#pendulum.run_simple_pendulum_example(l=1.0)
+#pendulum.run_simple_pendulum_example(l=2.0)
 
 print 'test double pendulum animation'
-pendulum.run_double_pendulum_example_with_animation()
+# pendulum.run_double_pendulum_example_with_animation()
 
 print 'test parameter learning'
 l = 1.1
@@ -20,3 +20,8 @@ learn_pendulum_parameter.plot_F(th_0, th_1, th_2, l)
 l_learn = learn_pendulum_parameter.learn_length_from_three_angle(th_0, th_1, th_2, l_init=1.0)
 print 'theoritical value ', l
 print 'learnt value value ', l_learn
+
+angles = np.loadtxt('hypercolumn/angle_sequence.npy')
+
+l_pend = learn_pendulum_parameter.learn_length_from_sequence(angles)
+print 'pendulum length', l_pend
