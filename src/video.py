63dc87dda4df8542bfb4fdd3006136437de429d1
Louis THIRY <louis.thiry@outlook.fr>
2017-01-12 22:48:05 +0100

    modify script to analize different layers

--- src/video.py
+++ src/video.py
@@ -18,11 +18,11 @@ from utils import get_activations, get_video_number_of_frames
 
 
 
-def write_activations_from_video(input_video='../pendule.mp4', layer_index=15, activations_file='data/activations.npy'):
+def write_activations_from_video(input_video='../pendule.mp4', layer_indexes=[15], activations_files=['data/activations.npy']):
 
     convolutionnal_neural_network = VGG_16()
     n_frames = get_video_number_of_frames(input_video)
-    activations = []
+    activations = [[] for _ in range(len(layer_indexes))]
 
     cap = cv2.VideoCapture(input_video)
 
@@ -38,16 +38,17 @@ def write_activations_from_video(input_video='../pendule.mp4', layer_index=15, a
 
         convolutionnal_neural_network.predict(image_CNN)
 
-        activations.append(get_activations(convolutionnal_neural_network, layer_index, image_CNN)[0][0])
+        for i, layer_index in enumerate(layer_indexes):
+            activations[i].append(np.array(get_activations(convolutionnal_neural_network, layer_index, image_CNN)[0][0]))
 
         print 'frame ', f, ' done'
 
-    activations = np.array(activations)
-    np.save(activations_file, activations)
+    for i in range(len(activations)):
+        activations_i = np.array(activations[i])
+        np.save(activations_files[i], activations_i)
+        print 'activation file ' + activations_files[i] + ' written'
 
-    print 'activation file ' + activations_file + ' written'
-
-def write_activation_video(activations_file='data/activations.npy', neuron_index=0, output_video='../activation_videos/activation_video.mp4', N_FPS=15, n_frames=None):
+def write_activation_video(activations_file='data/activations.npy', neuron_indexes=[0], output_videos=['../activation_videos/activation_video.mp4'], N_FPS=15, n_frames=None):
     activations = np.load(activations_file)
 
     if (n_frames is None):
@@ -59,12 +60,14 @@ def write_activation_video(activations_file='data/activations.npy', neuron_index
     FFMpegWriter = animation.writers['ffmpeg']
     writer = FFMpegWriter(fps=N_FPS)
 
-    fig = plt.figure()
 
-    with writer.saving(fig, output_video, 100):
-        for f in range(n_frames):
-            print 'writing frame ', f
-            im = activations[f, neuron_index]
-            plt.imshow(im)
-            writer.grab_frame()
+    for (neuron_index, output_video) in zip(neuron_indexes, output_videos):
+        fig = plt.figure()
+        with writer.saving(fig, output_video, 100):
+            for f in range(n_frames):
+                print 'writing frame ', f
+                im = activations[f, neuron_index]
+                plt.imshow(im)
+                writer.grab_frame()
+            print output_video, ' done'
 
